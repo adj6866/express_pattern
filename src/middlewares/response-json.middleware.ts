97b562b5043b2b5011eeb8115f7e87e5046d50df
adj6866 <ahmad.djunaedi@sera.astra.co.id>
2024-08-08 14:07:46 +0700

    fixing response default json

--- src/middlewares/response-json.middleware.ts
+++ src/middlewares/response-json.middleware.ts
@@ -1,40 +1,38 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpStatus } from '@/constants/http-status.contant';
 
-export function ResponseJson(req: Request, res: Response, next: NextFunction) {
-  const originalResponse = res.send.bind(res);
+export function ResponseJson(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const originalSend = res.send.bind(res);
 
-  if (req.originalUrl == '/efe9c963-6e6d-4a3e-9a38-332ce62f4c79') {
+  if (req.originalUrl === '/efe9c963-6e6d-4a3e-9a38-332ce62f4c79') {
     return next();
   }
 
   res.send = (body: any) => {
-    if (
-      res.statusCode >= 200
-      && res.statusCode < 300
-    ) {
-      const httpCode = body.httpCode || 200;
+    const { httpCode = 200, data = null, page } = body;
+
+    if (httpCode >= 200 && httpCode < 300) {
       res.set('Content-Type', 'application/json');
       res.status(httpCode);
 
-      const result = {
+      const responseBody = {
         transactionId: '0f06b466-99dd-4f59-a5df-1ad9f2a84d0a',
         code: '',
-        message: HttpStatus[body.httpCode] || 'OK',
+        message: HttpStatus[httpCode] || 'OK',
         eTag: 'pfmKgK6RpIkgkAAYukTfo21KRTyCwpiA',
-        data: body.data || null
-      }
+        data,
+        ...(page && { page })
+      };
 
-      const page = body.page || null;
-      if (page !== null) {
-        result['page'] = body.page;
-      }
-
-      return originalResponse(JSON.stringify(result));
+      return originalSend(JSON.stringify(responseBody));
     }
 
-    return originalResponse(body);
-  }
+    return originalSend(body);
+  };
 
   next();
 }
